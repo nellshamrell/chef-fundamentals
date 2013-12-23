e2c50b1cc4134f84344d61d05e8579bbd5f05c0b
Nell Shamrell <nshamrell@bluebox.net>
2013-12-22 17:43:46 -0800

    different default

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -8,19 +8,37 @@
 #
 
 # Install Apache
-package "apache2" do
+package_name = "apache2"
+service_name = "apache2"
+document_root = "/var/www"
+
+if node["platform"] == "centos"
+  package_name = "httpd"
+  service_name = "httpd"
+  document_root = "/var/www/html"
+end
+
+package package_name do
   action :install
 end
 
 # start the Apache service
 
-service "apache2" do
+service service_name do
   action [:enable, :start]
 end
 # make sure the service starts on reboot
 
 # write our homepage
-cookbook_file "/var/www/index.html" do
+=begin
+cookbook_file "#{document_root}/index.html" do
   source "index.html"
   mode "0644"
 end
+=end
+
+template "#{document_root}/index.html" do
+  source "index.html.erb"
+  mode "0644"
+end
+
